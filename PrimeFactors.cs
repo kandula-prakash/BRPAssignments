32e557605ec70231d7d808f8f3ae2a8332c7315f
kandula-prakash <abhinayprakash143@gmail.com>
2021-10-29 22:32:10 +0530

    feat:7day

---
+++ PrimeFactors.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day7
+{
+    class PrimeFactors
+    {
+        public void findingPrimeFactors()
+        {
+            int Num;
+            Console.WriteLine("Enter Number of which you want to print factors =");
+            Num = Convert.ToInt32(Console.ReadLine());
+
+
+
+            Console.WriteLine("Prime factors are ");
+            for (int i = 2; i < Num; i++)
+            {
+
+                if (Num % i == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace day7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrimeFactors primeFactors = new PrimeFactors();
+            primeFactors.findingPrimeFactors();
+
+            SecondLargestAndSmallest secondLargest = new SecondLargestAndSmallest();
+            secondLargest.FindSecondLargest();
+
+
+            Binary binary = new Binary();
+            binary.findingBinary();
+        }
+    }
+}
