c6ec08dc81f99c11459209c01faa7a0f2733d906
kandula-prakash <abhinayprakash143@gmail.com>
2021-10-29 22:45:11 +0530

    feat:day9

---
+++ Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrpDay9
+{
+    class Factorial
+    {
+        public static void CalculateFactorial()
+        {
+            int product = 1;
+            Console.WriteLine("Enter the number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                product = product * i;
+            }
+            Console.WriteLine("Factorial of " + number + " is" + product);
+        }
+    }
+}
